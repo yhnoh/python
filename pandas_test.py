0c7592ad3a16a49769ff8c554ea45b79abe54735
yhnoh <tedgom19@gmail.com>
2025-09-08 22:20:38 +0900

    pandas index 알아보기

--- pandas_test.py
+++ pandas_test.py
@@ -19,6 +19,52 @@ class MyTestCase(unittest.TestCase):
         data_frame = pd.DataFrame(arr)
         print('dataFrame =', data_frame)
 
+    def test_create_dataframe(self):
+        arr = np.array([
+            ['name1', 10, 160, 'M'],
+            ['name2', 12, 170, 'F'],
+            ['name3', 15, 180, 'M']
+        ])
+
+        df = pd.DataFrame(arr, columns=['name', 'age', 'height', 'sex'])
+        print('dataFrame =\n', df)
+
+        dict = {
+            'name': ['name1', 'name2', 'name3'],
+            'age': [10, 12, 15],
+            'height': [160, 170, 180],
+            'sex': ['M', 'F', 'M']
+        }
+
+        df = pd.DataFrame(dict)
+        print('dataFrame =\n', df)
+
+    def test_index_dataframe(self):
+        """
+        """
+        dict = {
+            'name': ['name1', 'name2', 'name3'],
+            'age': [10, 12, 15],
+            'height': [160, 170, 180],
+            'sex': ['M', 'F', 'M']
+        }
+
+        df = pd.DataFrame(dict)
+        print("dataFrame =\n", df)
+        print("dataFrame Index=\n",df.index)
+
+        """
+        인덱스 지정, 컬럼을 기반으로 인덱스 지정 가능
+        """
+        # df = df.set_index('name')
+        ## 인덱스 지정, 컬럼을 기반으로 인덱스 지정 가능
+        set_index = df.set_index('name')
+        print("dataFrame Index=\n", set_index)
+        print('dataframe loc =\n', set_index.loc['name1'])
+        ## 기본 인덱스 복원
+        print('dataframe =\n', df)
+        print("dataFrame Index=\n", df.reset_index())
+
 
 if __name__ == '__main__':
     unittest.main()
