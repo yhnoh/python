74017c62363cc7befa90417d81f0867bf0eb3474
yhnoh <tedgom19@gmail.com>
2025-09-15 23:20:14 +0900

    pandas dataframe concat, merge, join 알아보기

--- pandas_test.py
+++ pandas_test.py
@@ -162,5 +162,203 @@ class MyTestCase(unittest.TestCase):
         print(df_eval)
         display(df[df_eval])
 
+    def test_concat_dataframe(self):
+        """
+        Dataframe Concatenate
+        두개 이상의 Dataframe을 하나의 Dataframe으로 합치는 기능
+        axis를 지정하여 여러 Dataframe을 하나로 합치는 것이 가능
+        """
+
+        data1 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                 'Age': [27, 24, 22, 32],
+                 'Address': ['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['Msc', 'MA', 'MCA', 'Phd']}
+
+        data2 = {'Name': ['Abhi', 'Ayushi', 'Dhiraj', 'Hitesh'],
+                 'Age': [17, 14, 12, 52],
+                 'Address': ['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['Btech', 'B.A', 'Bcom', 'B.hons']}
+
+        df1 = pd.DataFrame(data1, index=[0, 1, 2, 3])
+        df2 = pd.DataFrame(data2, index=[4, 5, 6, 7])
+
+        print()
+        print("concat() 메서드를 이용한 DataFrame 합치기")
+        display(pd.concat([df1, df2]))
+
+
+        ## 서로 다른 컬럼 및 동일한 인덱스를 가진 DataFrame 합치기
+
+        data1 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                 'Age': [27, 24, 22, 32],
+                 'Address': ['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['Msc', 'MA', 'MCA', 'Phd'],
+                 'Mobile No': [97, 91, 58, 76]}
+
+        data2 = {'Name': ['Gaurav', 'Anuj', 'Dhiraj', 'Hitesh'],
+                 'Age': [22, 32, 12, 52],
+                 'Address': ['Allahabad', 'Kannuaj', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['MCA', 'Phd', 'Bcom', 'B.hons'],
+                 'Salary': [1000, 2000, 3000, 4000]}
+
+        df1 = pd.DataFrame(data1, index=[0, 1, 2, 3])
+
+        df2 = pd.DataFrame(data2, index=[2, 3, 6, 7])
+
+        ## Axis 옵션을 통한 컬럼 나열
+        print()
+        print("concat() 메서드를 이용한 DataFrame 합치기 > axis 옵션을 사용한 join outer를 통한 컬럼 나열")
+        display(pd.concat([df1, df2], axis=1))
+
+        print()
+        print("concat() 메서드를 이용한 DataFrame 합치기 > axis 옵션을 사용한 join inner를 통한 컬럼 나열")
+        display(pd.concat([df1, df2], axis=1, join="inner"))
+
+        print()
+        print("concat() 메서드를 이용한 DataFrame 합치기 > ignore_index 옵션을 사용한 인덱스 재설정")
+        display(pd.concat([df1, df2], ignore_index=True))
+
+        data1 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                 'Age': [27, 24, 22, 32],
+                 'Address': ['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['Msc', 'MA', 'MCA', 'Phd']}
+
+        data2 = {'Name': ['Abhi', 'Ayushi', 'Dhiraj', 'Hitesh'],
+                 'Age': [17, 14, 12, 52],
+                 'Address': ['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['Btech', 'B.A', 'Bcom', 'B.hons']}
+
+        df1 = pd.DataFrame(data1, index=[0, 1, 2, 3])
+        df2 = pd.DataFrame(data2, index=[4, 5, 6, 7])
+
+        print()
+        print("concat() 메서드를 이용한 DataFrame 합치기 > keys 옵션을 사용한 MultiIndex 생성")
+        display(pd.concat([df1, df2], keys=["x", "y"]))
+
+
+        data1 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                 'Age': [27, 24, 22, 32],
+                 'Address': ['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['Msc', 'MA', 'MCA', 'Phd']}
+
+        df = pd.DataFrame(data1, index=[0, 1, 2, 3])
+        s1 = pd.Series([1000, 2000, 3000, 4000], name='Salary')
+
+        print()
+        print("concat() 메서드를 이용한 DataFrame과 Series 합치기")
+        display(pd.concat([df, s1], axis=1))
+
+    def test_merge_dataframe(self):
+        """
+        Dataframe Merging
+        SQL의 JOIN과 유사한 기능을 제공하며 특정 컬럼을 기준으로 합치는 기능 제공
+        """
+
+        data1 = {'key': ['K0', 'K1', 'K2', 'K3'],
+                 'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                'Age':[27, 24, 22, 32],}
+        data2 = {'key': ['K0', 'K1', 'K2', 'K3'],
+                 'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                'Qualification':['Btech', 'B.A', 'Bcom', 'B.hons']}
+
+        df1 = pd.DataFrame(data1)
+        df2 = pd.DataFrame(data2)
+
+        print()
+        print("merge() 메서드를 이용한 DataFrame 합치기 > on 옵션을 통해서 어떤 컬럼을 기준으로 합칠지 지정")
+        display(pd.merge(df1, df2, on='key'))
+
+        data1 = {'key': ['K0', 'K1', 'K2', 'K3'],
+                 'key1': ['K0', 'K1', 'K0', 'K1'],
+                 'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                'Age':[27, 24, 22, 32],}
+        data2 = {'key': ['K0', 'K1', 'K2', 'K3'],
+                 'key1': ['K0', 'K0', 'K0', 'K0'],
+                 'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                'Qualification':['Btech', 'B.A', 'Bcom', 'B.hons']}
+        df1 = pd.DataFrame(data1)
+        df2 = pd.DataFrame(data2)
+
+        print()
+        print("merge() 메서드를 이용한 DataFrame 합치기 > on 옵션을 이용한 MultiKey Merge")
+        display(pd.merge(df1, df2, on=['key', 'key1']))
+
+
+        data1 = {'key': ['K0', 'K1', 'K2', 'K3'],
+                 'key1': ['K0', 'K1', 'K0', 'K1'],
+                 'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                'Age':[27, 24, 22, 32],}
+
+        data2 = {'key': ['K0', 'K1', 'K2', 'K3'],
+                 'key1': ['K0', 'K0', 'K0', 'K0'],
+                 'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+                'Qualification':['Btech', 'B.A', 'Bcom', 'B.hons']}
+
+        df1 = pd.DataFrame(data1)
+        df2 = pd.DataFrame(data2)
+
+        print()
+        print("merge() 메서드를 이용한 DataFrame 합치기 > how 옵션을 이용한 다양한 Join")
+        pd.merge(df1, df2, how='left', on=['key', 'key1'])
+        pd.merge(df1, df2, how='right', on=['key', 'key1'])
+        pd.merge(df1, df2, how='outer', on=['key', 'key1'])
+        pd.merge(df1, df2, how='inner', on=['key', 'key1'])
+
+    def test_join_dataframe(self):
+        """
+        Dataframe Joining
+        SQL의 JOIN과 유사한 기능을 제공하며 특정 인덱스를 기준으로 합치는 기능 제공
+        """
+
+        data1 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                 'Age': [27, 24, 22, 32]}
+        data2 = {'Address': ['Allahabad', 'Kannuaj', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['MCA', 'Phd', 'Bcom', 'B.hons']}
+
+        df1 = pd.DataFrame(data1, index=['K0', 'K1', 'K2', 'K3'])
+        df2 = pd.DataFrame(data2, index=['K0', 'K2', 'K3', 'K4'])
+
+        print()
+        print("join() 메서드를 이용한 DataFrame 합치기 > 인덱스 기준")
+        display(df1.join(df2))
+
+        print()
+        print("join() 메서드를 이용한 DataFrame 합치기 > how 옵션을 이용한 다양한 조인 방식")
+        display(df1.join(df2, how='inner'))
+
+        data1 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+                 'Age': [27, 24, 22, 32],
+                 'Key': ['K0', 'K1', 'K2', 'K3']}
+
+        data2 = {'Address': ['Allahabad', 'Kannuaj', 'Allahabad', 'Kannuaj'],
+                 'Qualification': ['MCA', 'Phd', 'Bcom', 'B.hons']}
+
+        df1 = pd.DataFrame(data1)
+        df2 = pd.DataFrame(data2, index=['K0', 'K2', 'K3', 'K4'])
+
+        print()
+        print("join() 메서드를 이용한 DataFrame 합치기 > on 옵션을 활용한 인덱스가 아닌열 기준으로 합치기")
+        print(df1.join(df2, on="Key"))
+
+
+        data1 = {'Name': ['Jai', 'Princi', 'Gaurav'],
+                 'Age': [27, 24, 22]}
+        data2 = {'Address': ['Allahabad', 'Kannuaj', 'Allahabad', 'Kanpur'],
+                 'Qualification': ['MCA', 'Phd', 'Bcom', 'B.hons']}
+
+        df1 = pd.DataFrame(data1, index=pd.Index(['K0', 'K1', 'K2'], name='key'))
+
+        multiple_index = pd.MultiIndex.from_tuples([('K0', 'Y0'), ('K1', 'Y1'),
+                                           ('K2', 'Y2'), ('K2', 'Y3')],
+                                          names=['key', 'Y'])
+
+        df2 = pd.DataFrame(data2, index=multiple_index)
+
+        print()
+        print("join() 메서드를 이용한 DataFrame 합치기 > MultiIndex 합치기")
+        print(df1.join(df2))
+
+
+
 if __name__ == '__main__':
     unittest.main()
